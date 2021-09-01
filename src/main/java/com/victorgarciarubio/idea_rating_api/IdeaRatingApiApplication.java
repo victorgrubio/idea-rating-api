c1901b5e61524c36c9dde65a4267b8ebfb8a0b93
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2021-09-01 08:09:29 +0200

    Init Spring Project

---
+++ src/main/java/com/victorgarciarubio/idea_rating_api/IdeaRatingApiApplication.java
@@ -0,0 +1,13 @@
+package com.victorgarciarubio.idea_rating_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaRatingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaRatingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorgarciarubio/idea_rating_api/IdeaRatingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorgarciarubio.idea_rating_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaRatingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
